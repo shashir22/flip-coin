7481a59e7e12c604ee9133f110152dff8f701e57
Shashikumar <shashirgowdas67@gmail.com>
2022-02-14 00:46:01 +0530

    Added:Display winner in the flip coin

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace flipcoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to flip coin program");
+            
+            string[] coin = { "head", "tail" };
+
+            int head = 0;
+
+            Random random = new Random();
+            int flipCoin = random.Next(0, 2);
+            if (flipCoin == head)
+            {
+                Console.WriteLine("head");
+            }
+            else
+            {
+                Console.WriteLine("tail");
+            }
+            Console.ReadLine();
+        }
+    }
+}
