5fef30779d5a214de7f4222228179c5ceb800e7a
Shashikumar <shashirgowdas67@gmail.com>
2022-02-14 00:52:48 +0530

    Added:Flip coin shows multiple counts

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace flipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the flip coin program");
+
+           
+            const int head = 0;
+            const int tail = 1;
+
+            //initilization varaibles
+            int headCount = 0;
+            int tailCount = 0;
+            int i = 0;
+
+
+
+            while (i <= 21)
+            {
+
+                Random random = new Random();
+                int flipResult = random.Next(0, 2);
+                if (flipResult == head)
+                {
+                    headCount++;
+
+                }
+                else
+                { 
+                if (flipResult == tail)
+
+                    tailCount++;
+
+                }
+                i++;
+            }
+            Console.WriteLine($"Head {headCount}");
+
+            Console.WriteLine($"Tail {tailCount}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
